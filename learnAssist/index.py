24c8d3a4f485cf8047ea3c8dc4a4c9eaee14ffb6
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-20 12:49:52 +0200

    development

---
+++ learnAssist/index.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+
+
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student, related_name='courses')
+
+
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+
+
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+# Optimizing queries with prefetching
+products = Product.objects.prefetch_related('category')
