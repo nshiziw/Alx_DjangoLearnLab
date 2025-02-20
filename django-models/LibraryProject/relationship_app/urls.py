4e2561e34d75a247ad50976400dade1a2cab41d8
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-20 16:33:24 +0200

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,25 +1,3 @@
-from .views import list_books, admin, LibraryDetailView
-from django.urls import path, include
-
-urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # Incl
-]
-
-from django.urls import path
-from .views import register, user_login, user_logout, list_books, LibraryDetailView
-
-urlpatterns = [
-    path('', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-]
-
-
 from django.urls import path
 from .views import register, list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
