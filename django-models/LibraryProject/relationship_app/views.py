849b64c38202f9a3394dca48046594ba49e77880
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-20 13:17:36 +0200

    .

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,3 +11,7 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
+def list_books(request):
+    books = Book.objects.select_related('author').all()  
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Updated path
