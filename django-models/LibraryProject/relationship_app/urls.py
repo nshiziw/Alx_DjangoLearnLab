3cc4a69ae5a848ea624c03e12db1745739e9ccdd
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-22 10:15:54 +0200

    development

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,16 +10,15 @@ urlpatterns = [
     path('register/', 'relationship_app.views.register', name='register'),
 
     # Book management paths
-    path('', list_books, name='list_books'),  # URL for listing books
     path('add/', add_book, name='add_book'),  # URL for adding a book
-    path('edit/<int:book_id>/', edit_book, name='edit_book'),  # URL for editing a specific book
+    path('edit/<int:book_id>/', edit_book, name='edit_book/'),  # URL for editing a specific book
     path('delete/<int:book_id>/', delete_book, name='delete_book'),  # URL for deleting a specific book
 
     # Views for different roles
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-    
+
     # Other paths
     path('library/<int:pk>/', 'relationship_app.views.LibraryDetailView', name='library_detail'),
 ]
