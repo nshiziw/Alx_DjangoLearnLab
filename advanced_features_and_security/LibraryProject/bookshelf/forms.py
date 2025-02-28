4c0f4f85321eae21efd7c4fe9d8f502c42d20423
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-28 14:50:04 +0200

    development

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -17,3 +17,14 @@ class BookForm(forms.ModelForm):
         if len(author) < 3:
             raise forms.ValidationError("Author name must be at least 3 characters long.")
         return author
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if len(name) < 2:
+            raise forms.ValidationError("Name must be at least 2 characters long.")
+        return name
