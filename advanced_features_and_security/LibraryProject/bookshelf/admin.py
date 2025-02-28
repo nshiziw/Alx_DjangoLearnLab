84a4bbe2d3842461eb9451faac2037a50230052b
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-28 14:26:24 +0200

    development

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -6,3 +6,25 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")  # Columns shown in list view
     search_fields = ("title", "author")  # Enables search by title and author
     list_filter = ("publication_year",)  # Adds filtering by publication year
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'profile_photo', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('username', 'email', 'password1', 'password2')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
