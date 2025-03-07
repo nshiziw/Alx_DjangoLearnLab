c94b6f15e9799276e8be9ced6785ea454fcf22fb
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-07 22:44:29 +0200

    developmnet

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,3 +8,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include API routes
 ]
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Initialize the router
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('', include(router.urls)),  # Include the router URLs for CRUD operations
+]
