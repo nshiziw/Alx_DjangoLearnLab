598dfeee3dd36ad456c8711a60836726b9e9bffe
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-11 01:00:35 +0200

    development

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Get book by ID
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book (without pk)
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book (without pk)
 ]
