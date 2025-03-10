f46d4ee50afd8f4594386390ef2a76ad3a5643de
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-11 01:02:40 +0200

    development

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # Create your views here.
 from rest_framework import generics, permissions
 from .models import Book
