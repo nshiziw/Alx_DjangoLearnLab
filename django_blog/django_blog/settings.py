400d7f91c540b6a3508edea402f682406eb86c05
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-18 14:11:19 +0200

    development

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PORT': '3306',
+        'NAME': 'blogs',
+        'PASSWORD': ''
     }
 }
 
