3fa2665c0ca6bf0c693e2ecd4ad1102b7b8170bd
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-18 14:20:12 +0200

    development

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("profile/", views.profile, name="profile"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
+
+
+urlpatterns += [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
