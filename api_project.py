b254f979e30366280c1d082359a5695fe83b0bb6
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-18 13:52:44 +0200

    development

--- api_project.py
+++ api_project.py
@@ -14,7 +14,7 @@ class Book(models.Model):
 from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Book
         fields = '__all__'
