785b20896c102be0bb2be0e469759f8302b9b4ae
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-30 20:44:48 +0200

    development

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,9 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=255)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    bio = serializers.CharField(required=False, allow_blank=True)
+    profile_picture = serializers.ImageField(required=False)
+
     class Meta:
         model = get_user_model()
         fields = ['username', 'password', 'bio', 'profile_picture']
+
+    def validate_password(self, value):
+        # Validate password using Django's default password validation system
+        validate_password(value)
+        return value
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
