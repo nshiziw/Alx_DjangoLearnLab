41079ed832fcf4183ef4e38fb532229644f949bd
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-30 21:16:13 +0200

    development

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,23 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
 # accounts/views.py
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, UserSerializer
-from rest_framework import permissions, status
+from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from .models import CustomUser  # Import CustomUser
 
-class RegisterUserView(CreateAPIView):
+class RegisterUserView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
-class LoginView(APIView):
+class LoginView(generics.GenericAPIView):  # Using generics.GenericAPIView explicitly
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
@@ -29,17 +23,16 @@ class LoginView(APIView):
         return Response({'error': 'Invalid credentials'}, status=400)
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
-    queryset = get_user_model().objects.all()
+    queryset = CustomUser.objects.all()  # Explicitly using CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
 
-
 @api_view(['POST'])
 def follow_user(request, user_id):
-    user_to_follow = get_user_model().objects.filter(id=user_id).first()
+    user_to_follow = CustomUser.objects.filter(id=user_id).first()
     if not user_to_follow:
         return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
     
@@ -51,7 +44,7 @@ def follow_user(request, user_id):
 
 @api_view(['POST'])
 def unfollow_user(request, user_id):
-    user_to_unfollow = get_user_model().objects.filter(id=user_id).first()
+    user_to_unfollow = CustomUser.objects.filter(id=user_id).first()
     if not user_to_unfollow:
         return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
 
